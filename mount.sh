f18b0d5d41d1ed79b5e71d8eb5b7ca114141023b
alperkesen <kesen.alper@gmail.com>
2019-12-09 14:10:41 +0300

    Add ansilove lib in quick mount

--- mount.sh
+++ mount.sh
@@ -1,3 +1,3 @@
-gcc txt2png.c -o txt2png -Wall -ansi -W -std=c99 -g -ggdb -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -lfuse -lmagic
+gcc txt2png.c -o txt2png -Wall -ansi -W -std=c99 -g -ggdb -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -lfuse -lmagic -lansilove
 ./txt2png -d $(pwd)/src $(pwd)/dst
  
